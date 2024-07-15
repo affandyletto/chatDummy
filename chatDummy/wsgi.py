d9431a2144da81a57a1dd1465ec37f4b38e87938
Affandy Letto <letto.affandy@yahoo.com>
2024-07-15 16:37:19 +0700

    'second'

--- chatDummy/wsgi.py
+++ chatDummy/wsgi.py
@@ -5,7 +5,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
+
 
 import os
 
@@ -13,4 +13,20 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatDummy.settings')
 
+application = get_wsgi_application()
+
+"""
+# +++++++++++ DJANGO +++++++++++
+# To use your own Django app use code like this:
+import os
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/letto2024/chatDummy/chatDummy'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'chatDummy.settings'
+
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
